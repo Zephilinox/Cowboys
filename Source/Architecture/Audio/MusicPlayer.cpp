bab32cf0e31d90209266c502202722c8c5b913a8
Zephilinox <zephilinox@hotmail.co.uk>
2018-03-30 00:38:29 +0100

    turned off music for now

--- Source/Architecture/Audio/MusicPlayer.cpp
+++ Source/Architecture/Audio/MusicPlayer.cpp
@@ -2,6 +2,9 @@
 
 void MusicPlayer::play(std::string music_name)
 {
+	//music really sucks when you have two games running on the same machine
+	return;
+
 	if (music_name != current_music)
 	{
 		current_music = music_name;
