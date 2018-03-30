9150c3c75aa345e86c9915ed07ea228550a11c62
Zephilinox <zephilinox@hotmail.co.uk>
2018-03-30 20:49:17 +0100

    Mute music JSON

--- Source/Architecture/Audio/MusicPlayer.cpp
+++ Source/Architecture/Audio/MusicPlayer.cpp
@@ -1,9 +1,33 @@
 #include "MusicPlayer.hpp"
 
+//STD
+#include <fstream>
+
+//LIBS
+#include <jsoncons/json.hpp>
+
+MusicPlayer::MusicPlayer()
+{
+	try
+	{
+		std::ifstream file("../../Resources/settings.json");
+		jsoncons::json settings;
+		file >> settings;
+		muted = settings["Audio"]["Music Muted"].as_bool();
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << "ERROR INFO: " << e.what() << "\n";
+	}
+}
+
 void MusicPlayer::play(std::string music_name)
 {
-	//music really sucks when you have two games running on the same machine
-	return;
+	//todo: improve this so we can mute midgame
+	if (muted)
+	{
+		return;
+	}
 
 	if (music_name != current_music)
 	{
